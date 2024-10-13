b640136f94ac3dbb652332939e3b90bdcb1e46db
bubna92 <136169348+LPopivnyak@users.noreply.github.com>
2024-10-13 18:19:52 +0300

    fullcity

--- shuter/character_body_3d.gd
+++ shuter/character_body_3d.gd
@@ -37,8 +37,23 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+var rot_y = 0.0
+var rot_x = 0.0
+var rot = 0.1
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 func _input(event):
+	
+	if event is  InputEventMouseMotion:
+		rot_y -= event.relative.x * rot
+		rot_x -= event.relative.y * rot
+		transform.basis = Basis(Vector3(0,1,0), rot_y)
+		
+	if Input.is_action_just_pressed("mouse_change"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 	if Input.is_action_just_pressed("get gun"):
 		anima_player.play("get_gun_001")
 		GUN = true
